b1e4c526a571d515211936ee1ea37d3a487b153b
Vicente Gonzalez <vgonzalez28@gmail.com>
2018-02-11 09:13:41 -0800

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,7 @@ $(document).ready(function (){
     $(".theme-button").on("click", function() {
       audioElement.play();
     });
+    
 
     $(".pause-button").on("click", function() {
       audioElement.pause();
@@ -30,18 +31,20 @@ $(document).ready(function (){
         for (var i=0; i< charArr.length; i++){
             var num = (Math.floor(12/charArr.length))
             var charImage = $("<div class='card-deck myChar col-md-"+num+
-            "' value='"+i+ "'style='width:14rem;'><div class='card-img-top'><img src='"+charArr[i].image+"' style='width:150px;height:170px;'/></div><div class='card-body'><div class='card-text'>"+charArr[i].name+"<br>"+"HP: "+charArr[i].HP+"<br>"+"Attack Power: "+charArr[i].attackpower+ "<br>"+"Counter Attack Power: "+charArr[i].counterattack+"</div></div></div>")
+            "' value='"+i+ "'style='width:14rem;'><div class='card-img-top'><img src='"+charArr[i].image+"' style='width:150px;height:170px;'/></div><div class='card-body'><div class='card-text'>"+charArr[i].name+"<br>"+"HP: "+charArr[i].HP+"</div></div></div>")
             $("#charlist").append(charImage)
             
         }
     }
 
+    
+    
     $(document).on("click",".myChar",function(){
         if(isHeroChosen===false){
             ChosenHero = charArr[$(this).attr("value")]
             $(this).addClass("fader")
             console.log(ChosenHero.name)
-            $(".hero-choice").append("<img src='"+ChosenHero.image+"' 'style='width:180px;height:200px;'/>"+"<br>"+"HP: "+ ChosenHero.HP+ "<br>"+ "Attack Power: "+ ChosenHero.attackpower+"<br>"+"Counter Attack Power: "+ ChosenHero.counterattack)
+            $(".hero-choice").append("<img src='"+ChosenHero.image+"' 'style='width:180px;height:200px;'/>"+"<br>"+"<div class='hero-HP'>"+'HP: '+ ChosenHero.HP+"</div>")
 
             isHeroChosen = true
         }
@@ -49,50 +52,76 @@ $(document).ready(function (){
             ChosenEnemy = charArr[$(this).attr("value")]
             $(this).addClass("fader")
             console.log(ChosenEnemy.name)
-            $(".enemy-choice").append("<img src='"+ChosenEnemy.image+"''style='width:180px;height:200px;'/>"+"<br>"+" HP: "+ ChosenEnemy.HP+ "<br>"+ "Attack Power: "+ ChosenEnemy.attackpower+"<br>"+"Counter Attack Power: "+ChosenEnemy.counterattack)
+            $(".enemy-choice").append("<img src='"+ChosenEnemy.image+"''style='width:180px;height:200px;'/>"+"<br>"+"<div class='enemy-HP'>"+" HP: "+ ChosenEnemy.HP+"</div>" )
             isEnemyChosen = true
         }
     })
 
     $(".button").on("click",function(){
         attackbutton.play();
-        console.log(ChosenHero.name + " vs "+ ChosenEnemy.name)
-        if(ChosenHero.HP>0 && ChosenEnemy.HP>0){
-            console.log(chosenHero.HP)
-        }
-        // else{
+        $(".hero-HP").html("HP: "+(ChosenHero.HP -= ChosenEnemy.counterattack()))
+        $(".enemy-HP").html("HP: "+(ChosenEnemy.HP -= ChosenHero.attackpower()))
+        if(ChosenHero.HP <= 0){
+            alert("Game Over, You Lose!")
+            isEnemyChosen = false
+            $(".enemy-choice").empty()
+            isHeroChosen = false
+            $(".hero-choice").empty()
+            $(".hero-HP").reset()
+            $(".enemy-HP").html("HP: "+100)
+            
 
-        //     alert("Game Ends")
-        // }
+        }
+        else if(ChosenEnemy.HP <= 0){
+            alert("You Beat " + ChosenEnemy.name+"!")
+            // $(document).reset()
+            isEnemyChosen = false
+            $(".enemy-choice").empty()
+            $("hero-HP").html("HP: "+100)
+
+        }
+        else{
+        }
+       
     })
+
+   
 
         var charArr = [
             {
                 name:"Sheldon",
                 HP: 100,
-                attackpower: 10,
-                counterattack: 50,
+                attackpower: function(){
+                    return Math.floor(Math.random()*10)+1},
+                counterattack: function(){
+                    return Math.floor(Math.random()*10)+1},
                 image: "./assets/images/Sheldon.png"
             },
             {
                 name:"Leonard",
-                HP: 110,
-                attackpower: 40,
-                counterattack: 30,
+                HP: 100,
+                attackpower:function(){
+                    return Math.floor(Math.random()*10)+1},
+                counterattack:function(){
+                    return Math.floor(Math.random()*10)+1},
                 image: "./assets/images/Leonard.png"
             },
             {
                 name:"Howard",
-                HP: 90,
-                attackpower: 35,
-                counterattack: 25,
+                HP: 100,
+                attackpower: function(){
+                    return Math.floor(Math.random()*10)+1},
+                counterattack: function(){
+                    return Math.floor(Math.random()*10)+1},
                 image: "./assets/images/Howard.png"
             },
             {
                 name:"Raj",
-                HP: 95,
-                attackpower: 30,
-                counterattack: 45,
+                HP: 100,
+                attackpower: function(){
+                    return Math.floor(Math.random()*10)+1},
+                counterattack: function(){
+                    return Math.floor(Math.random()*10)+1},
                 image: "./assets/images/Raj.png"
             }
                 
